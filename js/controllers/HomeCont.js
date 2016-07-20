7289ce0c4b72937b530a411665b3e0204deada39
CEREBR4L <mattmillergraphics@gmail.com>
2016-07-21 00:49:24 +0100

    Adding to home

--- js/controllers/HomeCont.js
+++ js/controllers/HomeCont.js
@@ -1,4 +1,8 @@
 angular.module('port')
 	.controller('HomeCont', function HomeController(){
-		this.title = "HOME PAGE, HIYA!";
+		this.heroMain = "WELCOME TO MY PORTFOLIO";
+		this.heroSub = "You will find all you need to know about me here.";
+		this.projectsHeader = "Projects";
+		this.skillsHeader = "Skills";
+		this.aboutHeader = "About Me";
 	});
