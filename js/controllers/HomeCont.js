ea94f51d25f4d8e252c7841612499edeba9aa2d8
CEREBR4L <mattmillergraphics@gmail.com>
2016-07-22 00:44:18 +0100

    Layout and style update

--- js/controllers/HomeCont.js
+++ js/controllers/HomeCont.js
@@ -1,7 +1,7 @@
 angular.module('port')
 	.controller('HomeCont', function HomeController(){
-		this.heroMain = "WELCOME TO MY PORTFOLIO";
-		this.heroSub = "You will find all you need to know about me here.";
+		this.heroMain = "CEREBRAL'S PORTFOLIO";
+		this.heroSub = "You will find all you need to know about me and my work here.";
 		this.projectsHeader = "Projects";
 		this.skillsHeader = "Skills";
 		this.aboutHeader = "About Me";
